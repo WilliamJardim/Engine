3300e020a2c08ad0f93ecb625df0f1be54847ca9
William Jardim <williamalvesjardim@gmail.com>
2025-03-09 15:48:58 -0300

    Criando abas ativaveis e desativaveis com conceito de toggle usando valores booleano

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
 // src/App.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import ThreeScene from './ThreeScene';
 import DebugTerminal from './editor/DebugTerminal';
 import SceneBuilder from './editor/SceneBuilder';
-import './DivComponentes.css'
+import './DivComponentes.scss'
 
 const App: React.FC = () => {
+  const [sceneEnabled, setSceneEnabled] = useState(true);
+  const [terminalEnabled, setTerminalEnabled] = useState(false);
+  const [builderEnabled, setBuilderEnabled] = useState(false);
+
+  function onSceneClick()
+  { 
+    setSceneEnabled( !sceneEnabled );
+  }
+
+  function onTerminalClick()
+  {
+    setTerminalEnabled( !terminalEnabled );
+  }
+
+  function onBuilderClick()
+  {
+    setBuilderEnabled( !builderEnabled );
+  }
+
   return (
     <div className='div-principal'>
       <h1>Three.js com React e TypeScript</h1>
 
       <div className='div-abas'>
-        <button> Scene </button>
-        <button> Terminal </button>
-        <button> Builder </button>
+        <button className={ sceneEnabled    ? 'enabled' : 'disabled' } onClick={ onSceneClick }    > Scene    </button>
+        <button className={ builderEnabled  ? 'enabled' : 'disabled' } onClick={ onBuilderClick }  > Builder  </button>
+        <button className={ terminalEnabled ? 'enabled' : 'disabled' } onClick={ onTerminalClick } > Terminal </button>
       </div>
 
       <div className='div-componentes'>
-        <ThreeScene />
-        <DebugTerminal/>
-        <SceneBuilder/>
+        {
+          sceneEnabled && <ThreeScene />
+        }
+        {
+          terminalEnabled && <DebugTerminal />
+        }
+        {
+          builderEnabled && <SceneBuilder />
+        }
       </div>
 
     </div>

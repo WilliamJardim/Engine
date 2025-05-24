043b2bd532a5f30925aa229e5bc2589bb25836fc
William Jardim <williamalvesjardim@gmail.com>
2025-05-24 11:41:45 -0300

    Planejando estrutura de rastreamento de frames

---
+++ src/engine/interfaces/ObjectFrameTracker/ObjectFrameData.ts
@@ -0,0 +1,15 @@
+/**
+* AUTHOR: William Alves Jardim
+* LICENSE: Creative Commons BY-NC-ND 4.0 (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+* 
+* Desenvolvido por William Alves Jardim como parte de um projeto pessoal.
+* Protegido por direitos autorais e licenciado sob Creative Commons BY-NC-ND 4.0.
+* 
+* Veja o arquivo `LICENSE` na raiz do repositório para mais detalhes.
+*/
+export default interface ObjectFrameData{
+    frameDelta: number,
+    frameNumber: number,
+    firstRender: number,
+    renderizadorPronto: number
+}
